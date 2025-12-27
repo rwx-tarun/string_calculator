6c4b68e57f55afa053af94e71d7d77cf8ee39ae8
rwx-tarun <78414267+rwx-tarun@users.noreply.github.com>
2025-12-27 11:17:29 +0530

    Added single digit sum test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,3 +2,11 @@ from string_calculator.calculator import add
 
 def test_empty_string_returns_zero():
     assert add("") == 0
+
+
+def test_single_number_returns_value():
+    assert add("1") == 1
+
+
+def test_two_numbers_return_sum():
+    assert add("1,5") == 6
