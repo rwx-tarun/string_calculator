c92a2f470090208883de15d6180a2b244ae8c55e
rwx-tarun <78414267+rwx-tarun@users.noreply.github.com>
2025-12-27 12:01:25 +0530

    check negatives Update test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -10,3 +10,11 @@ def test_single_number_returns_value():
 
 def test_two_numbers_return_sum():
     assert add("1,5") == 6
+
+
+def test_custom_delimiter():
+    assert add("//;\n1;2") == 3
+
+def test_multiple_negatives_in_exception_message():
+    with pytest.raises(ValueError, match="negative numbers not allowed"):
+        add("1,-1,2,-3")
